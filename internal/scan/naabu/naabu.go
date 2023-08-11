186ef92c809920912484ce5eab61c8484922a339
Liam Somerville <liam@liso.ca>
2023-08-11 09:36:34 -0600

    feat: closes #34

--- internal/scan/naabu/naabu.go
+++ internal/scan/naabu/naabu.go
@@ -5,41 +5,84 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"time"
 )
 
 type NaabuScan struct {
-	Hosts []*Host
+	V1Hosts []*HostV1
+	V2Hosts []*HostV2
 }
 
-type Host struct {
+type HostV1 struct {
 	Name      string `json:"host"`
 	IPAddress string `json:"ip"`
 	Port      int    `json:"port"`
 }
 
-func Parse(scan []byte) (*NaabuScan, error) {
+type HostV2 struct {
+	Name      string    `json:"host"`
+	IPAddress string    `json:"ip"`
+	Port      Port      `json:"port"`
+	Timestamp time.Time `json:"timestamp"`
+}
+
+type Port struct {
+	Port     int  `json:"Port"`
+	Protocol int  `json:"Protocol"`
+	TLS      bool `json:"TLS"`
+}
+
+func ParseV1(scan []byte) (*NaabuScan, error) {
 	s := &NaabuScan{}
 	// Naabu scan is JSONL, so we have to read it line by line
 	sc := bufio.NewScanner(bytes.NewReader(scan))
 	sc.Split(bufio.ScanLines)
 	for sc.Scan() {
-		h := &Host{}
-		err := json.Unmarshal(sc.Bytes(), &h)
+		hv1 := &HostV1{}
+		err := json.Unmarshal(sc.Bytes(), &hv1)
 		if err != nil {
-			return nil, fmt.Errorf("error unmarshaling Naabu: %v", err)
+			return nil, fmt.Errorf("error unmarshaling naabu V1: %v", err)
 		}
-		if isMissingKeys(h) {
+		if isMissingV1Keys(hv1) {
 			return nil, fmt.Errorf("missing Naabu keys")
 		}
-		s.Hosts = append(s.Hosts, h)
+		s.V1Hosts = append(s.V1Hosts, hv1)
 	}
 	return s, nil
 }
 
-// isMissingKeys attempts to filter non-Naabu JSON files (e.g. Amass output)
-func isMissingKeys(h *Host) bool {
+func ParseV2(scan []byte) (*NaabuScan, error) {
+	s := &NaabuScan{}
+	// Naabu scan is JSONL, so we have to read it line by line
+	sc := bufio.NewScanner(bytes.NewReader(scan))
+	sc.Split(bufio.ScanLines)
+	for sc.Scan() {
+		hv2 := &HostV2{}
+		err := json.Unmarshal(sc.Bytes(), &hv2)
+		if err != nil {
+			return nil, fmt.Errorf("error unmarshaling naabu V2: %v", err)
+		}
+		if isMissingV2Keys(hv2) {
+			return nil, fmt.Errorf("missing Naabu keys")
+		}
+		s.V2Hosts = append(s.V2Hosts, hv2)
+	}
+	return s, nil
+}
+
+// isMissingV1Keys attempts to filter non-Naabu JSON files (e.g. Amass output)
+func isMissingV1Keys(h *HostV1) bool {
 	// Every host should have one of these
 	if h.IPAddress == "" || h.Port == 0 {
+		return true
+	}
+	return false
+}
+
+// isMissingV2Keys attempts to filter non-Naabu JSON files (e.g. Amass output)
+func isMissingV2Keys(h *HostV2) bool {
+	// Every host should have one of these
+	if h.IPAddress == "" || h.Port.Port == 0 {
 		return true
 	}
 	return false
